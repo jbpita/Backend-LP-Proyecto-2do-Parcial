f3ac521de379d0a5ef97d6de326a8897fa07e59e
luiaabar <luiaabar@espol.edu.ec>
2021-08-24 20:46:46 -0500

    Se anadio ciertas lineas que faltaban para la creacion del modelo

--- src/modelo/cargarDocumentos.js
+++ src/modelo/cargarDocumentos.js
@@ -1,5 +1,6 @@
 const modeloHotel = require('./db_Hotel');
 const modeloHospital = require('./db_Hospital');
+const modeloRestaurante = require('./db_Restaurante');
 
 const controlador = require('../controlador/cargaDatos');
 const {storage} = require('../confirguracion/configuracion_firebase');
@@ -8,6 +9,7 @@ const {storage} = require('../confirguracion/configuracion_firebase');
 const inicializarDB  = () => {
     controlador.subirDocumento('HOTELES' , modeloHotel);
     controlador.subirDocumento('HOSPITALES' , modeloHospital); 
+    controlador.subirDocumento('RESTAURANTES' , modeloRestaurante);
 }
  
 module.exports.inicializarDB = inicializarDB;
