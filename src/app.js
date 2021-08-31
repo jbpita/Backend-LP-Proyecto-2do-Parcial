5124c9e2bf3c57a492f6220fa19d569fd0ba2924
luiaabar <luiaabar@espol.edu.ec>
2021-08-31 18:55:26 -0500

    Configuracion de acceso de distintas fuentes al servidor

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {inicializarDB} = require('./modelo/cargarDocumentos');
 const apiRouter = require('./rutas/rutas');
 const indexRouter = require('./rutas/index');
+const cors = require('cors');
 
 app = express();
 //configuracion exprress
@@ -9,7 +10,16 @@ app.set("port" , process.env.PORT || 8000);
 
 
 //middleware
+app.use((req , res , next) => {
 
+    res.header('Access-Control-Allow-Origin' , '*');
+
+    res.header('Access-Control-Allow-Methods' , 'GET,PUT,POST,DELETE');
+
+    next();
+
+})
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/api', apiRouter );
 
--- src/rutas/rutas.js
+++ src/rutas/rutas.js
@@ -8,5 +8,5 @@ const router = express.Router();
 
 router.use('/hoteles' , hotelRouter);//jorge hizo esto  
 router.use('/hospitales', hospitalRouter); // Edwars realizó esto: tabla de hospital, su modelo, cargar documento, y ruta
-router.use('/restaurantes', restauranteRouter)
+router.use('/restaurantes', restauranteRouter) // Componente del backend trabajado por Luis
 module.exports = router; 
