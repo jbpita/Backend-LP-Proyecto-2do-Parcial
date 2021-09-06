0b698674fb68ecbd3ff6c2b8fdd2a43a2cfec90d
jbpita <jbpita@espol.edu.ec>
2021-09-05 21:00:48 -0500

    Se modifica las rutas d elas imagenes de las base de datos de hoteles

--- src/app.js
+++ src/app.js
@@ -2,15 +2,25 @@ const express = require('express');
 const {inicializarDB} = require('./modelo/cargarDocumentos');
 const apiRouter = require('./rutas/rutas');
 const indexRouter = require('./rutas/index');
+const cors = require('cors');
 
 app = express();
 //configuracion exprress
 app.set("port" , process.env.PORT || 8000);
-
+app.use((req , res , next) => {
+    res.header('Access-Control-Allow-Origin' , '*');
+    res.header('Access-Control-Allow-Methods' , 'GET,PUT,POST,DELETE');
+    next();
+})
 
 //middleware
+app.use(cors());
 
-app.use('/', indexRouter);
+//var whitelist = ['https://api.flutlab.io/res/projects/284941/0i74celterbiupvbkjng/index.html'];
+
+
+
+app.use('/',indexRouter);
 app.use('/api', apiRouter );
 
 //incializacion db
