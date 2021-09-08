92366a7516a292acf6084eb2de457e610b0484b3
jbpita <jbpita@espol.edu.ec>
2021-09-07 21:49:08 -0500

    Se agrega el crud del registro para reserva de hoteles, y se guarda en firebase

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const {inicializarDB} = require('./modelo/cargarDocumentos');
 const apiRouter = require('./rutas/rutas');
 const indexRouter = require('./rutas/index');
 const cors = require('cors');
+var body_parser = require('body-parser');
 
 app = express();
 //configuracion exprress
@@ -20,6 +21,10 @@ app.use((req , res , next) => {
 
 })
 app.use(cors());
+//app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//app.use(body_parser.urlencoded({extended:true}));
+
 app.use('/', indexRouter);
 app.use('/api', apiRouter );
 
--- src/rutas/hoteles.js
+++ src/rutas/hoteles.js
@@ -1,9 +1,71 @@
 const { cargarDocumentos } = require('../controlador/cargaDatos');
+const {db} = require('../confirguracion/configuracion_firebase');
 var express = require('express');
+var querystring = require('querystring');
+
 var router = express.Router();
 
 router.get('/', (req, res) => {
     cargarDocumentos('HOTELES' , res);
+});
+
+router.post('/form' , (req , res) => {
+    const registro = req.body;
+    if(!registro){
+        return res.status(400).json({error: 'No hay datos'});
+    }
+    const reserva = {
+        nombre: registro.nombre,
+        correo: registro.correo, 
+        telefono: registro.telefono, 
+        personas_alojarse: registro.personas_alojarse, 
+        fecha_llegada: registro.fecha_llegada, 
+        fecha_salida: registro.fecha_salida
+    };
+    db.collection("RegistroHoteles").add(reserva)
+                .then( (docRef) => {
+                    console.log("Se creo la reserva con ID: " ,docRef.id);
+                    res.status(201).json({
+                        success: true,
+                        content: {
+                            reserva
+                        }
+                    })
+                })
+                .catch( (error) => {
+
+                });
+    /*
+    if(req.method === 'POST'){
+        let data_post = '';
+        req.on('data' , (data) => {
+            data_post += data;
+        });
+        req.on('end' , () => {
+            let data_post_objeto = querystring.parse(data_post);
+            const registro = {
+                nombre: data_post_objeto.nombre,
+                correo: data_post_objeto.correo, 
+                telefono: data_post_objeto.telefono, 
+                personas_alojarse: data_post_objeto.personas_alojarse, 
+                fecha_llegada: data_post_objeto.fecha_llegada, 
+                fecha_salida: data_post_objeto.fecha_salida
+            };
+            db.collection("RegistroHoteles").add(registro)
+                .then( (docRef) => {
+                    console.log("Se creo la reserva con ID: " ,docRef.id);
+                    req.status(201).json({
+                        success: true,
+                        content: {
+                            registro
+                        }
+                    })
+                })
+                .catch( (error) => {
+
+                });
+        });
+    }*/
 });
 
 module.exports = router;
